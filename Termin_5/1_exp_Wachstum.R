17cafdcd4c2e346a4b1550469b4700f3fcc5f414
Till Francke <francke@uni-potsdam.de>
2016-11-02 09:51:06 +0100

    Struktur, Ideen für Einführung in R

---
+++ Termin_5/1_exp_Wachstum.R
@@ -0,0 +1,24 @@
+# Seminar zu "Einführung in die Modellierung/Integrierte Modellierung"
+# 1.11.2016
+
+# Populationswachstum, exponentielles Modell
+
+# n0 : Startpopulation
+# r  : Wachstumsrate
+# nt : Anzahl Zeitschritte
+  
+  n0 = 2
+  r  = 1.1
+  nt = 100
+  
+  #Variablen vorbereiten
+  n = rep(NA,nt)
+  n[1] = n0
+  
+  # Iterationsschleife
+  for (t in 2:nt)
+    n[t] = r * n[t-1]
+
+  
+  plot(1:nt, n, xlab = "Zeitschritte", ylab="Populationsgröße")
+  
