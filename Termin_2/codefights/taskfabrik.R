b65c172285114c77ea340e52e286ca24d699fc64
Till Francke <francke@uni-potsdam.de>
2016-12-09 10:53:27 +0100

    restored code and tasks of Termin_2 and Termin_3, which I somehow had screwed up

---
+++ Termin_2/codefights/taskfabrik.R
@@ -0,0 +1,120 @@
+# Task 01
+wuerfel = function(n_wuerfe, seed) {
+  set.seed(seed)
+  wuerfe = ceiling(runif(n = n_wuerfe, min = 0, max=6))
+  return (wuerfe)
+}
+
+wuerfel(1, 1)
+wuerfel(5, 3)
+wuerfel(7, 0)
+wuerfel(3, 1000)
+
+# Task 02
+wuerfel_gezinkt = function(ergebnis) {
+  n_wuerfe = length(ergebnis)
+  testwurf = rep(0, n_wuerfe)
+  seed=0
+  while(any(testwurf != ergebnis))
+  {
+    seed=seed+1
+    set.seed(seed)
+    testwurf = sample(x = 1:6, size = n_wuerfe, replace = TRUE)
+  }
+
+  return (seed)
+}
+
+wuerfel_gezinkt(1)
+
+wuerfel_gezinkt(c(6,6))
+
+wuerfel_gezinkt(c(1,2,3,4))
+
+wuerfel_gezinkt(c(1,1,1,1,1))
+
+
+
+
+
+# Task 03
+schwimmbad = function(N0, r, N_grenzwert) {
+  
+  exp_Wachstum = function (N0, r, nt)
+  {  
+    # N0 : Startpopulation
+    # r  : Wachstumsrate
+    # nt : Anzahl Zeitschritte
+    
+    N = rep(NA,nt) #Vektor für zeitlichen Verlauf der Population anlegen
+    N[1] = N0
+    
+    for (t in 2:nt)
+      N[t] = N[t-1] + r * N[t-1]
+    
+    return(N)
+  }
+  
+  N = exp_Wachstum (N0=N0, r=r, nt=1000)
+  sperrtag = which (N > N_grenzwert)[1]
+  
+  if (is.na(sperrtag)) sperrtag = -1 #Grenzwert wurde nicht überschritten, gib -1 zurück
+  
+  return (sperrtag)
+}
+
+schwimmbad(2, 0.1, 100)
+schwimmbad(50, 0.01, 200)
+schwimmbad(5, 0.001, 1000)
+schwimmbad(10, 1.2, 100)
+schwimmbad(200, 0.1, 100)
+
+
+# Task 04
+schwimmbad2 = function(N0, r, N_grenzwert, r_chlor) {
+  
+  exp_Wachstum = function (N0, r, nt)
+  {  
+    # N0 : Startpopulation
+    # r  : Wachstumsrate
+    # nt : Anzahl Zeitschritte
+    
+    N = rep(NA,nt) #Vektor für zeitlichen Verlauf der Population anlegen
+    N[1] = N0
+    
+    for (t in 2:nt)
+      N[t] = N[t-1] + r * N[t-1]
+    
+    return(N)
+  }
+  
+  N = exp_Wachstum (N0=N0, r=r, nt=1000)
+  sperrtag = which (N > N_grenzwert)[1]
+  
+  if (is.na(sperrtag)) return(round(max(N)))   #Grenzwert wurde nicht überschritten, gib Maximalpopulation zurück
+  
+  N_chlor = exp_Wachstum (N0=N[sperrtag], r=r_chlor, nt=4)  #Wachstum unter Chloreinfluss
+  
+  return (round(N_chlor[4]))
+}
+
+schwimmbad2(2, 0.1, 100, -0.1)
+schwimmbad2(50, 0.01, 200, 0)
+schwimmbad2(5, 0.001, 1000, -0.1)
+schwimmbad2(10, 1.2, 100, 0.3)
+schwimmbad2(200, 0.1, 100, 0.01)
+
+
+# Task 05
+interpolation = function(t_mess, x_mess, t_approx) {
+return(approx(x=t_mess, y = x_mess, xout = t_approx)$y)
+}
+
+interpolation(c(0,1), c(0,30), 0.5)
+interpolation(c(0, 1, 2.5), c(0,30, 31), c(0.5, 1, 1.1))
+interpolation(c(2, 4, 5, 8) , c(1, 2, 5, 10), c(2.5, 2.5, 3))
+interpolation(c(-1,2), c(2,3), 0)
+
+
+  
+
---
+++ Termin_3/code/6_log_Wachstum_umwelt_gekoppelt.R
@@ -0,0 +1,162 @@
+# Seminar zu "Einführung in die Modellierung/Integrierte Modellierung"
+# 1.11.2016
+
+# Populationswachstum, exponentielles Modell mit Umweltstochastizität
+
+
+kapazitaet = function (K_opt, K_pess, theta_opt, praeferendumsbreite, theta)
+{
+  K = approx(x=c(theta_opt + praeferendumsbreite*c(-1.5, -0.5, 0.5, 1.5)), y = c(K_pess, K_opt, K_opt, K_pess), xout = theta, rule = 2)$y
+  return(K)
+}
+
+
+# Funktion "log_Wachstum_umwelt_gekoppelt" definieren
+#<
+log_Wachstum_umwelt_gekoppelt = function (n0, r_max, nt, K_opt, K_pess, theta_opt, praeferendumsbreite,
+                                          theta_A, theta_B, n_min=2, m)
+#>
+{  
+  # n0 :            Startpopulation
+  # r_max:   : maximale Wachstumsrate für aktuellen Zeitschritt
+  # nt :            Anzahl Zeitschritte
+  # K_opt, K_pess: Habitatkapazitäten unter optimalen bzw. Pessimum-Bedingungen
+  # theta_opt: Schwellenwerte der Umweltfaktoren (z.B. Bodenfeuchte) zu o.g. Kapazitäten
+  # praeferendumsbreite: Spannweite um das Optimum, in der die maximale Kapazität herrscht
+
+  # theta_A:   Zeitreihe des Umweltfaktors für Population A
+  # theta_B:   Zeitreihe des Umweltfaktors für Population B
+  
+  # n_min: minimal überlebensfähige Population
+  # m:  Migrationswahrscheinlichkeit
+
+
+  #Variable n vorbereiten: Matrix mit 2 Zeilen (Populationen A und B) und nt Spalten Tipp: "array()"
+  #<
+  n = array(NA,c(2,nt)) #Matrix für zeitlichen Verlauf der Population
+  #>
+  n[,1] = n0 #Zuweisung der Anfangspopulation von A und B
+  
+  # Iterationsschleife
+  for (t in 2:nt)
+  {
+    # dichtegesteuerte Wachstumsrate (logistischer Regression)
+    # Umweltabhängige Kapazität  
+    # Migration  
+    # jeweils für Population A (speichern in n[1,]) und Population B (speichern in n[,2])
+    #<
+    
+    K_A = kapazitaet(K_opt = K_opt, K_pess=K_pess, theta_opt = theta_opt, praeferendumsbreite = praeferendumsbreite, theta = theta_A[t])
+    K_B = kapazitaet(K_opt = K_opt, K_pess=K_pess, theta_opt = theta_opt, praeferendumsbreite = praeferendumsbreite, theta = theta_B[t])
+    
+    r_A = r_max * (1 - n[1,t-1]/K_A)
+    r_B = r_max * (1 - n[2,t-1]/K_B)
+    
+    # #Migration
+    m_A =    m  * n[1, t-1]
+    m_B =    m  * n[2, t-1]
+
+
+    n[1,t] = n[1,t-1] + r_A * n[1,t-1] -m_A + m_B
+    n[2,t] = n[2,t-1] + r_B * n[2,t-1] +m_A - m_B
+    
+    #Migration
+    # m_A =    m * n[1, t]
+    # m_B =    m  * n[2, t]
+    # 
+    # n[1,t] = n[1,t]  -m_A + m_B
+    # n[2,t] = n[2,t]  +m_A - m_B
+    # 
+    
+        
+    if (n[1,t]<n_min) n[1,t]=0
+    if (n[2,t]<n_min) n[2,t]=0
+  }
+  #>
+  
+  # Ergebnis zurückgeben
+  return(n)
+}
+  
+#Populationsparameter
+  theta_opt = 0.7 #Lage des Optimums (Bodenfeuchte)
+  K_opt  = 100 #Habitatkapazität unter optimalen Bedingungen
+  K_pess = 1   #Habitatkapazität unter schlechtestmöglichen Bedingungen
+  praeferendumsbreite = 0.3 #Breite des Präferendums
+  r_max = 0.01 # maximale Reproduktionsrate
+  nt = 3000 # Anzahl der zu simulierenden Zeitschritte
+  n0 = c(20, 20) # Werte für Anfangspopulation für A und B
+  m = 0.01
+ 
+  # Umweltbedingung (hier synthetisch erzeugt)
+    # Initialisierung des Zufallsgenerators für reproduzierbaren Zufall
+    set.seed(3)
+    theta_min=0 #Minimum und Maximum der Umweltvariable
+    theta_max=1
+    theta_A = runif(n = nt, min=theta_min, max=theta_max)  
+    theta_A = lowess(theta_A, f=0.10)$y  #Glättung, damit sich ein kontinuierlicher Verlauf zeigt
+    theta_A = (theta_A - min(theta_A)) / diff(range(theta_A))* (theta_max-theta_min) + theta_min
+    
+    theta_B = runif(n = nt, min=theta_min, max=theta_max)  
+    theta_B = lowess(theta_B, f=0.10)$y  #Glättung, damit sich ein kontinuierlicher Verlauf zeigt
+    theta_B = (theta_B - min(theta_B)) / diff(range(theta_B))* (theta_max-theta_min) + theta_min
+    
+    theta_B = 1- theta_A
+    
+  #Funktion mit Populationsparametern aufrufen, Rückgabewert in n speichern
+  #<
+  n = log_Wachstum_umwelt_gekoppelt(n0 = n0, r_max=r_max, nt = nt, 
+                            K_opt = K_opt, K_pess = K_pess, 
+                            theta_opt=theta_opt, praeferendumsbreite = praeferendumsbreite,
+                            theta_A=theta_A, theta_B = theta_B, m=0.01, n_min = 2)
+  #>
+  
+  
+  #Populationen in einer Grafik darstellen
+  par(oma=c(0,0,0,1.5))
+  
+  plot  (x=1:length(n[1,]), y=n[1,], type="l", col="black", xlab = "Zeitschritte", ylab="Populationsgröße", ylim=c(0, max(n)))
+  lines (x=1:length(n[2,]), y=n[2,],           col="red")
+  
+  #weitere Grafik über die erstere legen, ohne diese zu löschen
+  par(new=TRUE)
+  plot(x=1:length(n[1,]), y = theta_A, col="black", type="l", lty=2, ylim = c(theta_min, 2*theta_max), axes = FALSE,
+       xlab="", ylab="" )
+  lines(x=1:length(n[2,]), y = theta_B, col="red", type="l", lty=2)
+  #zweite y-Achsenbeschriftung rechts einfügen (Tipp: axis(...))
+  axis(side=4)
+  #horizontale Linie bei theta_opt +/- praeferendumsbreite/2 einzeichnen
+  abline(h=c(theta_opt-praeferendumsbreite/2, theta_opt, theta_opt+praeferendumsbreite/2) , lty=3, col="black")
+  mtext(text="theta", side=4, outer = TRUE)
+  
+  legend("topright", legend=c("n_1", "n_2", "theta"), col=c("black", "red"), lty= c(1,1,2))
+ 
+  #<? Wie groß muss die Migrationsrate sein, um ein Aussterben beider Populationen innerhalb 
+  #<? des simulierten Zeitraums zu verhindern?
+  #<
+  n = log_Wachstum_umwelt_gekoppelt(n0 = n0, r_max=r_max, nt = nt, 
+                                    K_opt = K_opt, K_pess = K_pess, 
+                                    theta_opt=theta_opt, praeferendumsbreite = praeferendumsbreite,
+                                    theta_A=theta_A, theta_B = theta_B, m=0.0009, n_min = 2)
+  #>
+  
+  #<? Durch Ändern eines Parameters sollen beide Populationen die gleiche Dynamik zeigen.
+  #<? Welceh Möglichkeiten gibt es?
+  #<
+  n0 = c(0,0)
+  r_max = 0
+  praeferendumsbreite = 10
+  theta_A = theta_B
+  m > 0.1
+  
+  #>
+  
+  
+ 
+  
+  
+
+ 
+  
+ 
+ 
