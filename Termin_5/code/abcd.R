fd80a1133cc5a38d34a7f0fb6f1308ce3a44f8b4
heistermann <maik.heistermann@uni-potsdam.de>
2017-01-17 10:34:22 +0100

    Updated R-Code and slides for Termin 5.

--- Termin_5/code/abcd.R
+++ Termin_5/code/abcd.R
@@ -17,6 +17,7 @@ Y = function(W, a, b) {
   return(out)  
   #>
 }
+#   Graphische Darstellung
 #<
 W = seq(0,50,0.1)
 plot(W,  Y(W, 0.9, 10), type="l", lwd=2, 
@@ -37,50 +38,116 @@ legend("bottomright",
 #>
 
 
-# AUFGABE: Entwickle das vollstaendige abcd-Modell als Funktion.
-#   (siehe Ablauf auf Folie #15).
-#   Meteorologischer Antrieb: P, PET
-#   Parameter: a, b, c, d
-#   Return: Liste mit monatlichen Werten für Abfluss Q, 
-#           Direktabfluss QD, Basisabfluss QB, Verdunstung ET,
-#           Bodenwasserspeicher S, Grundwasserspeicher G
-#           
+# Workspace ausleeren
+rm(list = ls())
+
+
+# AUFGABE: Bearbeite die folgenden sechs Aufgaben
+#   auf Grundlage der untenstehenden Implementierung
+#   des abcd-Modells.
+#
+#   1. Beschreibe die Argumente der Funktion
+#      im Hinblick auf Datentyp und Bedeutung.
+#
+#   2. Um welchen Datentyp handelt es sich beim 
+#      Rueckgabewert der Funktion? Welche Informationen
+#      enthaelt dieser Rueckgabewert? (Nutze den "str"-Befehl)
+#
+#   3. Versuche den Code auf Grundlage der Gleichungen
+#      auf Folie #15 zu verstehen. An welchen Stellen werden
+#      Bodenfeuchte und Grundwasserspeicher aktualisiert?
+#
+#   4. Vor der Schleife werden Startwerte fuer S und G gesetzt.
+#      Was ist hier das Problem? Was bedeutet das fuer
+#      die ersten Zeitschritte?
+#
+#   5. Unter der abcd-Funktion findest Du Testdaten (P, PET, a, b, c, d)
+#      und ein paar Zeilen Code zur Darstellung von Niederschlag,
+#      Gesamtabfluss und Basisabfluss. Experimentiere mit P, PET und den
+#      Parametern und beschreibe die beobachteten Effekte.
+#
+#   6. Wie kannst Du ueberpruefen, ob die Massenbilanz des Modells
+#      ausgeglichen ist?
+           
 abcd = function(P, PET, a, b, c, d) {
-  #<
   # Definiere Dataframe, in den die Ergebnisse geschrieben werden
   dummy = rep(0., length(P))
-  out = data.frame(Q=dummy, QD=dummy, QB=dummy, ET=dummy, S=dummy, G=dummy)
+  out = data.frame(Q=dummy, QD=dummy, QB=dummy, 
+                   ET=dummy, S=dummy, G=dummy)
   # Definiere Startwerte fuer Boden- und Grundwasserspeicher
-  S = b/2.
+  S = 0.
   G = 0.
   for (i in 1:length(P)) {
     # Verfuegbares Wasser
     W = P[i] + S
     # "Verdunstbares Wasser"
     Y = (W + b)/(2*a) - sqrt( (((W+b)/(2*a))^2 - b*W/a) )
+    # Aufteilung von Y zwischen Bodenwasser und ET
+    x = exp(-PET[i]/b)
     # Neuer Bodenwasserspeicher
-    S = Y * exp(-PET[i]/b)
+    S = x * Y
     # Evapotranspiration
-    ET = Y * (1 + exp(-PET[i]/b))
+    ET = (1 - x) * Y
     # Direktabfluss
-    QD = (1-c)*(W-Y)
+    QD = (1 - c) * (W-Y)
     # Grundwasserneubildung
-    RG = c*(W-Y)
+    RG = c * (W-Y)
     # Basisabfluss
     QB = d * G
-    # Abfluss
+    # Aktualisierung des Grundwasserspeichers
+    G = G + RG - d * G
+    # Gesamtabfluss
     Q = QB + QD
-    G = (G + RG)/(1+d)
     # Schreibe Ergebnisse fuer diesen Zeitschritt in Dataframe
     out[i,] = c(Q, QD, QB, ET, S, G)    
   }
   return(out)
-  #>  
 }
 
+# 12 Monate Testdaten fuer Niederschlag (P)
+testP = c(0, 0, 0, 200, 0, 0, 200, 0, 0, 0, 0)
+# 12 Monate Testdaten fuer PET
+testPET = rep(100, length(testP))
+# Parameterwerte
+a = 0.95 
+b = 300
+c = 0.6
+d = 0.6
+# Funktionsaufruf von abcd wird in Ergebnisvariable geschrieben
+result = abcd(testP, testPET, a, b, c, d)
+# Vektor mit Zeitschritten (hier: einfach Monatsnummern)
+zeit = 1:nrow(result)
+# Graphische Darstellung der Ganglinien
+op = par(mar = c(5,5,2,5))
+#   Gesamtabfluss
+plot(zeit, result$Q, type="b", lwd=2, col="red", 
+     xlab="Zeitindex", ylab="Abfluss", ylim=c(0,20))
+#   Grundwasserabfluss
+lines(zeit, result$QB, lwd=2, col="blue", type="b")
+#   Vertikale Linien zur besseren Orientierung
+abline(v=zeit, col="grey")
+#   Sekundaerachse anlegen
+par(new = T)
+#   Niederschlag (ohne Ahcse)
+plot(zeit, testP, type="h", lwd=3, axes=F, xlab=NA, ylab=NA)
+#   Sekundaerachse zeichnen
+axis(side = 4)
+#   Achsenbeschriftung
+mtext(side = 4, line = 3, "Niederschlag")
+#   Legende definieren
+legend("topright", 
+       legend=c("Abfluss (gesamt)", "Abfluss (GW)", "Niederschlag"),
+       lwd=2, col=c("red","blue", "black"), cex=0.75)
 
-# AUFGABE: Lies den monatlichen MOPEX-Datensatz "02296750.monthly"
-#    fuer den Pegel "PEACE RIVER AT ARCADIA, FLA."
+# Pruefe Massenbilanz
+ET_Q_deltas = sum(result$ET, result$Q, 
+                  result$S[nrow(result)], result$G[nrow(result)])
+print(paste("P = ", sum(testP)))
+print(paste("ET + Q + deltaS + deltaG = ", ET_Q_deltas))
+
+
+# AUFGABE (optional): Lies den monatlichen MOPEX-Datensatz 
+#    "02296750.monthly" (Pegel "PEACE RIVER AT ARCADIA, FLA.")
 #    unter Nutzung der read.table Funktion ein. Schaue Dir
 #    Die Datei selbst in einem Text-Editor an (z.B. notepad++).
 #    Inspiziere den Datensatz mit dem "str"-Befehl und stelle
@@ -90,33 +157,44 @@ abcd = function(P, PET, a, b, c, d) {
 mopex = read.table("02296750.monthly", sep=";", header=TRUE, stringsAsFactors=FALSE)
 # Konvertiere String zu Datumsobjekt
 mopex$date = as.Date(mopex$date)
-# Struktuzr des Dataframes
+# Struktur des Dataframes
 str(mopex)
-#>
-plot(mopex$date, mopex$discharge, type="l", col="black", lwd=2)
+# Graphische Darstellung als Zeitreihe mit Datum
+zoominto = c(as.Date("1990-01-01"), as.Date("1995-01-01"))
+plot(mopex$date, mopex$discharge, type="l", col="black", lwd=2, 
+     xlim=zoominto, ylim = c(0,300), 
+     xlab = "Datum", ylab = "Q, P, ET (mm)", xaxt="n")
+axis.Date(1, at=mopex$date, format="%m/%y")
 lines(mopex$date, mopex$precip, type="l", col="blue", lwd=2)
-lines(mopex$date, mopex$PET, type="l", col="red", lwd=2)
+lines(mopex$date, mopex$pet, type="l", col="red", lwd=2)
+legend("topright", 
+       legend=c("Abfluss (mm)", "Niederschlag (mm)", "PET (mm)"),
+       lwd=2, col=c("red","blue", "black"), cex=0.75)
+#>
 
-
-# AUFGABE: Nutze nun den MOPEX-Datensatz zum Antrieb Deines 
+# AUFGABE (optional): Nutze nun den MOPEX-Datensatz zum Antrieb Deines 
 #   abcd-Modells. Stelle die Dynamik des Abflusses, der einzelnen
 #   Abflusskomponenten und der Zustandsvariablen graphisch dar.
 #   Experimentiere mit den Parametern a, b, c und d.
 #<
 # Parameter als Vektor
-params = c(a=0.99, b=500., c=0.4, d=0.5)
+params = c(a=0.99, b=500., c=0.6, d=0.5)
 # Ausfuehrung des Modells
 out = abcd(mopex$precip, mopex$pet, 
            params["a"], params["b"], params["c"], params["d"])
 
 # Graphische Darstellung der Abflusskomponenten
-zoominto = NULL #c(as.Date("1990-01-01"), as.Date("2000-01-01"))
+zoominto = c(as.Date("1990-01-01"), as.Date("1995-01-01"))
 plot(mopex$date, out$Q, type="n",
      xlim=zoominto,
      ylim=c(0, max(c(out$Q, mopex$discharge), na.rm=TRUE)),
-     xlab="Zeit", ylab="Abfluss")
+     xlab="Zeit", ylab="Abfluss", xaxt="n")
+axis.Date(1, at=mopex$date, format="%m/%y")
 lines(mopex$date, out$Q,  col="black", lwd=2)
-lines(mopex$date, out$QB, col="blue", lwd=2)
-lines(mopex$date, mopex$discharge, col="red")
+lines(mopex$date, out$QB, col="darkgrey", lwd=2)
+lines(mopex$date, mopex$discharge, col="red", lwd=2)
+legend("topright", 
+       legend=c("Abfluss (sim)", "Abfluss (obs)", "Basisabfluss (sim)"),
+       lwd=2, col=c("black", "red", "grey"), cex=0.75)
 #>
 
